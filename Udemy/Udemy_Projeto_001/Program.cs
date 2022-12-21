3afeebdbbcfd05b86f6afb2a05e526d993381f82
Julyane Pereira <87590342+julypereira@users.noreply.github.com>
2022-12-21 07:16:51 -0400

    CSharp - Iniciante
    Curso prático que ensina a linguagem C# para iniciantes. Curso ministrado pelo instrutor Antonio Henrique Reis na plataforma Udemy.

---
+++ Udemy/Udemy_Projeto_001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Projeto_001
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");        }
+    }
+}
