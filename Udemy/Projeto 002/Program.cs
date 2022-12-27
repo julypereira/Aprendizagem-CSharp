6d791aa2553aeae2997b617e5f029dff944e9b67
Julyane Pereira <87590342+julypereira@users.noreply.github.com>
2022-12-26 21:32:36 -0400

    Update

---
+++ Udemy/Projeto 002/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");        // string - cadeia de caracteres
+            Console.WriteLine('x');                 // char - um único caracter
+            Console.WriteLine(12345);               //inteiro
+            Console.WriteLine(123.45M);             //decimal
+            Console.WriteLine(123.45F);             //float
+            Console.WriteLine(123.45);              //double
+
+            Console.WriteLine("Número inteiro........:" + 12345);               //inteiro
+            Console.WriteLine("Número decimal........:" + 123.45M);             //decimal
+            Console.WriteLine("Número float..........:" + 123.45F);             //float
+            Console.WriteLine("Número double.........:" + 123.45);              //double
+
+
+
+
+        }
+    }
+}
---
+++ Udemy/Projeto 003/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração das variáveis:
+            string nomeFuncionario;
+            char opcaoUsuario;
+            int diaNascimento;
+            int mesNascimento;
+            int anoNascimento;
+            int idadeFuncionario;
+            decimal salarioBruto;
+            decimal valorDesconto;
+            bool maiorIdade;
+
+            // atribuir valores as variáveis:
+            nomeFuncionario = "Julyane Pereira da Silva";
+            Console.WriteLine("Nome do funcionário: " + nomeFuncionario);
+
+            opcaoUsuario = 's';
+            diaNascimento = 03;
+            mesNascimento = 08;
+            anoNascimento = 1987;
+            idadeFuncionario = 35;
+            salarioBruto = 50000.45M;
+            valorDesconto = 100.45M;
+            maiorIdade = true;
+
+
+            //apresentação dos dados:
+            Console.WriteLine("Dia do nascimento.........: " + diaNascimento);
+            Console.WriteLine("Mês do nascimento.........: " + mesNascimento);
+            Console.WriteLine("Ano do nascimento.........: " + anoNascimento);
+            Console.WriteLine("Idade do funcionário......: " + idadeFuncionario);
+            Console.WriteLine("Salário bruto.............: " + salarioBruto);
+            Console.WriteLine("Valor desconto............: " + valorDesconto);
+            Console.WriteLine("Maior de idade?...........: " + maiorIdade);
+            Console.WriteLine("Opção do usuário..........: " + opcaoUsuario);
+
+
+
+
+
+
+
+        }
+    }
+}
