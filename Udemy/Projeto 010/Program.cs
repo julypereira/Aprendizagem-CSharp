0ab3fc66dd38fd6ae437b4545b805aa65fbdb558
Julyane Pereira <87590342+julypereira@users.noreply.github.com>
2023-01-16 06:41:51 -0400

    Update

---
+++ Udemy/Projeto 010/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valorA, valorB, resultado;
+
+            valorA = 10;
+            valorB = 20;
+            resultado = valorA + valorB;
+
+            Console.WriteLine("somando o valor: "+valorA+" com o valor: "+valorB+" temos como resultado: "+resultado);
+            Console.WriteLine("Pressione uma tecla");
+            Console.ReadKey();
+
+        }
+    }
+}
