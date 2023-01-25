d6dc9632840d71cd618ec5ce820e9cf5927c04ae
Julyane Pereira <87590342+julypereira@users.noreply.github.com>
2023-01-25 06:55:03 -0400

    Update

---
+++ Udemy/Projeto 011/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("\nInteiro");
+
+            //int valorA = 100;
+            //int valorB = 20;
+
+            //int soma = valorA + valorB;
+            //int diferenca = valorA - valorB;
+            //int produto = valorA * valorB;
+            //int quociente = valorA / valorB;
+
+            //Console.WriteLine("A soma é..............:"+ soma);
+            //Console.WriteLine("A diferença...........:" + diferenca);
+            //Console.WriteLine("O produto é...........:" + produto);
+            //int valorA = 100;
+            //int valorB = 20;
+
+            //Console.WriteLine("\nDecimal");
+
+            //decimal valorA = 100.0M;
+            //decimal valorB = 20.33M;
+
+            //decimal soma = valorA + valorB;
+            //decimal diferenca = valorA - valorB;
+            //decimal produto = valorA * valorB;
+            //decimal quociente = valorA / valorB;
+
+            //Console.WriteLine("A soma é..............:"+ soma);
+            //Console.WriteLine("A diferença...........:" + diferenca);
+            //Console.WriteLine("O produto é...........:" + produto);
+            //Console.WriteLine("O quociente é.........:" + quociente);
+
+
+            //Console.WriteLine("\nFloat");
+
+            //float valorA = 100.0F;
+            //float valorB = 20.33F;
+
+            //float soma = valorA + valorB;
+            //float diferenca = valorA - valorB;
+            //float produto = valorA * valorB;
+            //float quociente = valorA / valorB;
+
+            //Console.WriteLine("A soma é..............:" + soma);
+            //Console.WriteLine("A diferença...........:" + diferenca);
+            //Console.WriteLine("O produto é...........:" + produto);
+            //Console.WriteLine("O quociente é.........:" + quociente);
+
+
+            Console.WriteLine("\nDouble");
+
+            double valorA = 100.0;
+            double valorB = 20.33;
+
+            double soma = valorA + valorB;
+            double diferenca = valorA - valorB;
+            double produto = valorA * valorB;
+            double quociente = valorA / valorB;
+
+            Console.WriteLine("A soma é..............:" + soma);
+            Console.WriteLine("A diferença...........:" + diferenca);
+            Console.WriteLine("O produto é...........:" + produto);
+            Console.WriteLine("O quociente é.........:" + quociente);
+
+
+            Console.WriteLine("\nPressione uma tecla");
+            Console.ReadKey();
+
+        }
+    }
+}
